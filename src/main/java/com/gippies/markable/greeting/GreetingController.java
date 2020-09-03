c1ee95f923a202ccd7ff20a162288a7275db8940
Joshua Patterson <jleegippies@gmail.com>
2020-09-03 18:17:19 -0500

    Added a greetings list page

--- src/main/java/com/gippies/markable/greeting/GreetingController.java
+++ src/main/java/com/gippies/markable/greeting/GreetingController.java
@@ -7,11 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class GreetingController {
 
     @Autowired
     private GreetingService service;
+
+    @GetMapping("/greetings")
+    public String greetings(Model model) {
+        List<Greeting> greetingList = service.listAll();
+        model.addAttribute("greetingList", greetingList);
+        return "greeting_list";
+    }
 
     @GetMapping("/greeting")
     public String greeting(Model model) {
