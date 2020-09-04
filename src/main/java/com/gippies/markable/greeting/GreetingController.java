6386245332fc55688619a3e10af641bcd291d07a
Joshua Patterson <jleegippies@gmail.com>
2020-09-03 19:42:55 -0500

    Switched greetingSubmit to use greeting_detail template

--- src/main/java/com/gippies/markable/greeting/GreetingController.java
+++ src/main/java/com/gippies/markable/greeting/GreetingController.java
@@ -33,7 +33,7 @@ public class GreetingController {
     public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
         model.addAttribute("greeting", greeting);
         service.save(greeting);
-        return "result";
+        return "greeting_detail";
     }
 
     @GetMapping("/greeting/{id}")
