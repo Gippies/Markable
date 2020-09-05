c75aa6a270c8ce99590869539a8e946c0cdc2e21
Joshua Patterson <jleegippies@gmail.com>
2020-09-04 20:30:24 -0500

    WIP Got logins working

--- src/main/java/com/gippies/markable/IndexController.java
+++ src/main/java/com/gippies/markable/IndexController.java
@@ -1,6 +1,8 @@
 package com.gippies.markable;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +10,26 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String successfulLogin() {
+        System.out.println("BLURG");
+        return "hello";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "login";
     }
 }
